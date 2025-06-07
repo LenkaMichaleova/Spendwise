8989692460e532ab1a713cb339663befcca04545
Lenka Michaleová <michaleova.lenka@gmail.com>
2025-06-07 11:53:52 +0200

    stylování containeru v HomePage

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -3,8 +3,17 @@ import './style.css';
 
 export const HomePage = () => {
   return (
-    <div className="container">
-        <h1>HomePage</h1>
+    <div className="spendings">
+        <h1>Spendings</h1>
+
+        <div className='spendings__items-box'>
+          <button>
+
+          </button>
+        </div>
+
+
+
         <SpeedDialTooltipOpen />
     </div>
   );
